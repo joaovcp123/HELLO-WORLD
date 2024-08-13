b6f60a41e38795357950bd492aa6086bbd699011
joaovcp123 <joao.pedroso@hotmail.com>
2024-08-13 16:53:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let nomeUsuario = "";
+let elemento = document.querySelector("#nome-usuario");
+
+
+while(nomeUsuario ==""){
+    nomeUsuario = prompt("Qual seu nome?");
+}
+
+
+
+if(nomeUsuario == null){
+
+    elemento.textContent = "seja muito bem-vindo.";
+    
+} else {
+    
+    elemento.textContent = nomeUsuario;
+
+}
